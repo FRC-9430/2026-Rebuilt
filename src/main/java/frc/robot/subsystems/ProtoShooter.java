1b6d826089e68cded147ee276b224aaa2c78a6a6
FRCteam9430 <frc.9430@outlook.com>
2026-01-11 14:39:58 -0800

    Update motor IDs

--- src/main/java/frc/robot/subsystems/ProtoShooter.java
+++ src/main/java/frc/robot/subsystems/ProtoShooter.java
@@ -7,9 +7,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ProtoShooter extends SubsystemBase {
 
-    SparkFlex mainMotor1 = new SparkFlex(10, SparkFlex.MotorType.kBrushless);
-    SparkFlex mainMotor2 = new SparkFlex(11, SparkFlex.MotorType.kBrushless);
-    SparkFlex subMotor = new SparkFlex(12, SparkFlex.MotorType.kBrushless);
+    SparkFlex mainMotor1 = new SparkFlex(31, SparkFlex.MotorType.kBrushless);
+    SparkFlex mainMotor2 = new SparkFlex(32, SparkFlex.MotorType.kBrushless);
+    SparkFlex subMotor = new SparkFlex(33, SparkFlex.MotorType.kBrushless);
 
     public double mainSpeed = 0.0;
     public double subSpeed = 0.0;
