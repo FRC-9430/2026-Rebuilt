52bb90af4f89c76b02fabfc191dc4f5ea5a98d40
IamIisI <titusj915@gmail.com>
2026-01-11 12:08:24 -0800

    Log Speed Values To Dashboard

--- src/main/java/frc/robot/subsystems/ProtoShooter.java
+++ src/main/java/frc/robot/subsystems/ProtoShooter.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkFlex;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ProtoShooter extends SubsystemBase {
@@ -42,6 +43,7 @@ public class ProtoShooter extends SubsystemBase {
         } else if (mainSpeed < 0.0) {
             mainSpeed = 0.0;
         }
+        SmartDashboard.putNumber("MainSpeed", mainSpeed);
     }
 
     public void setSubSpeed(double speed) {
@@ -51,6 +53,7 @@ public class ProtoShooter extends SubsystemBase {
         } else if (subSpeed < 0.0) {
             subSpeed = 0.0;
         }
+        SmartDashboard.putNumber("SubSpeed", subSpeed);
     }
 
 }
