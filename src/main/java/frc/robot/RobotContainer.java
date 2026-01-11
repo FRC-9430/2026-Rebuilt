d7b804054aa71e4edc514dd09e28cfa0bdce1797
IamIisI <titusj915@gmail.com>
2026-01-11 12:02:20 -0800

    Add button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,52 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ProtoShooter;
 
 public class RobotContainer {
+
+  CommandXboxController driverController = new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
+
+  ProtoShooter protoShooter = new ProtoShooter();
+
   public RobotContainer() {
     configureBindings();
+
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    driverController.povUp().onTrue(new InstantCommand(() -> {
+      protoShooter.setMainSpeed(protoShooter.mainSpeed + 0.05);
+    }));
+
+    driverController.povDown().onTrue(new InstantCommand(() -> {
+      protoShooter.setMainSpeed(protoShooter.mainSpeed - 0.05);
+    }));
+
+    driverController.povRight().onTrue(new InstantCommand(() -> {
+      protoShooter.setSubSpeed(protoShooter.subSpeed + 0.05);
+    }));
+
+    driverController.povLeft().onTrue(new InstantCommand(() -> {
+      protoShooter.setSubSpeed(protoShooter.subSpeed - 0.05);
+    }));
+
+    driverController.leftTrigger(0.5).whileTrue(new InstantCommand(() -> {
+      protoShooter.runBackward();
+    })).onFalse(new InstantCommand(() -> {
+      protoShooter.stopMotors();
+    }));
+
+    driverController.rightTrigger(0.5).whileTrue(new InstantCommand(() -> {
+      protoShooter.runForward();
+    })).onFalse(new InstantCommand(() -> {
+      protoShooter.stopMotors();
+    }));
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
