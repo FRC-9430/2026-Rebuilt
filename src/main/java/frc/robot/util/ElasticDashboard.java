9b11feb0bf46699c650516be09f62b9f57734e6d
maddie 3 <maddiedvillasenor@gmail.com>
2026-01-10 17:46:06 -0800

    Add SmartDashboard for Elastic

---
+++ src/main/java/frc/robot/util/ElasticDashboard.java
@@ -0,0 +1,11 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ElasticDashboard {
+
+    public ElasticDashboard(){
+        SmartDashboard.putString("tester", "hello world :)");
+
+    }
+}
