851bb4972778a648799f35e8176b500c060ed851
PeaceCatis <littlemissamayapie@gmail.com>
2026-01-13 17:46:05 -0800

    Created Subsystem file and added Climber subsystem. Updated and created dummy safe PID placeholders

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,4 +3,16 @@ package frc.robot;
 
 public final class Constants {
   
+public static final class PIDConstants{
+
+    public static final double kClimberkp = 0.1;
+    public static final double kClimberki = 0;
+    public static final double kClimberkd = 0;
+
+
+
+    
+}
+
+
 }
