f55814e43964e1bb5391f7573918bdbcef57b9b1
bigbruh84 <bigbruh840@gmail.com>
2026-01-14 18:06:07 -0800

    Add dummy safe PID values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,8 +6,8 @@ import edu.wpi.first.math.controller.PIDController;
 public final class Constants {
 
     public static final class ClimbingArmConstants {
-        public static final double kP = 0.001;
-        public static final double kI = 0;
-        public static final double kD = 0;
+        public static final double kP = 0.001; // TODO: Dummy value
+        public static final double kI = 0; // TODO: Dummy value
+        public static final double kD = 0; // TODO: Dummy value
     
 }
