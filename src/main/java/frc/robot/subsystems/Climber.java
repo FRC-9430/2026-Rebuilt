08f7332e81a53e654c7f5d5f059a65bd779f3cc9
maddie 3 <maddiedvillasenor@gmail.com>
2026-01-15 19:51:33 -0800

    Start building basic vision

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import frc.robot.Constants.ClimbingArmConstants;
@@ -8,4 +11,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
     PIDController climberController = new PIDController(ClimbingArmConstants.kP,ClimbingArmConstants.kI,ClimbingArmConstants.kD);
+    AprilTagFieldLayout layout = new AprilTagFieldLayout();
+
 }
