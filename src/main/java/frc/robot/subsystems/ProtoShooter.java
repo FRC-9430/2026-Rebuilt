7d0daadb8c59c63424fb2bd4f1fb59d7e9877669
FRCteam9430 <frc.9430@outlook.com>
2026-01-15 16:28:13 -0800

    Eable m1 and use neg speed
    -Titus

--- src/main/java/frc/robot/subsystems/ProtoShooter.java
+++ src/main/java/frc/robot/subsystems/ProtoShooter.java
@@ -1,47 +1,52 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkFlex;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ProtoShooter extends SubsystemBase {
 
     SparkFlex mainMotor1 = new SparkFlex(31, SparkFlex.MotorType.kBrushless);
-    SparkFlex mainMotor2 = new SparkFlex(32, SparkFlex.MotorType.kBrushless);
+    //SparkFlex mainMotor2 = new SparkFlex(32, SparkFlex.MotorType.kBrushless);
     SparkFlex subMotor = new SparkFlex(33, SparkFlex.MotorType.kBrushless);
 
     public double mainSpeed = 0.0;
     public double subSpeed = 0.0;
 
     public ProtoShooter() {
-
+        
     }
 
     public void runForward() {
-        mainMotor1.set(-mainSpeed);
-        mainMotor2.set(mainSpeed);
+        mainMotor1.set(mainSpeed);
+        //mainMotor2.set(-mainSpeed);
         subMotor.set(subSpeed);
     }
 
     public void runBackward() {
-        mainMotor1.set(mainSpeed);
-        mainMotor2.set(-mainSpeed);
+        mainMotor1.set(-mainSpeed);
+        //mainMotor2.set(mainSpeed);
         subMotor.set(-subSpeed);
     }
 
     public void stopMotors() {
         mainMotor1.stopMotor();
-        mainMotor2.stopMotor();
+        //mainMotor2.stopMotor();
         subMotor.stopMotor();
+    }
+
+    public void runAt(double speed) {
+        mainMotor1.set(speed);
+        //mainMotor2.set(-speed);
+        subMotor.set(speed);
     }
 
     public void setMainSpeed(double speed) {
         mainSpeed = speed;
         if (mainSpeed > 1.0) {
             mainSpeed = 1.0;
-        } else if (mainSpeed < 0.0) {
-            mainSpeed = 0.0;
+        } else if (mainSpeed < -1.0) {
+            mainSpeed = 1.0;
         }
         SmartDashboard.putNumber("MainSpeed", mainSpeed);
     }
@@ -50,8 +55,8 @@ public class ProtoShooter extends SubsystemBase {
         subSpeed = speed;
         if (subSpeed > 1.0) {
             subSpeed = 1.0;
-        } else if (subSpeed < 0.0) {
-            subSpeed = 0.0;
+        } else if (subSpeed < -1.0) {
+            subSpeed = -1.0;
         }
         SmartDashboard.putNumber("SubSpeed", subSpeed);
     }
