73e8672e544e24c148a01102a53bb83f0663524c
PeaceCatis <littlemissamayapie@gmail.com>
2026-01-15 19:45:53 -0800

    Changed names of PID varibles in  constants. Added absolute encoder as well as starting setup on motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,8 +6,8 @@ import edu.wpi.first.math.controller.PIDController;
 public final class Constants {
 
     public static final class ClimbingArmConstants {
-        public static final double kP = 0.001; // TODO: Dummy value
-        public static final double kI = 0; // TODO: Dummy value
-        public static final double kD = 0; // TODO: Dummy value
-    
+        public static final double kclimberP = 0.001; // TODO: Dummy value
+        public static final double kclimberI = 0; // TODO: Dummy value
+        public static final double kclimberD = 0; // TODO: Dummy value
+    }
 }
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,11 +1,33 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
 import frc.robot.Constants.ClimbingArmConstants;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    PIDController climberController = new PIDController(ClimbingArmConstants.kP,ClimbingArmConstants.kI,ClimbingArmConstants.kD);
+    private PIDController climberController = new PIDController(ClimbingArmConstants.kclimberP,ClimbingArmConstants.kclimberI,ClimbingArmConstants.kclimberD);
+
+    private SparkMax ClimberMotor1;
+    private SparkMax ClimberMotor2;
+    private AbsoluteEncoder ClimbingEncoder;
+public void ClimbingArmSubsystem() {}
+
+
+
+
+
+
+
+
+@Override
+  public void periodic() {
+
+
+climberController.calculate(ClimbingEncoder.getPosition(), 0.2);
+  }
 }
