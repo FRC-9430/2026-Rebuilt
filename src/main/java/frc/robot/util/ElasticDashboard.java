d9c29e66bbabd36409d1576b78992e05e8ffec7d
IamIisI <titusj915@gmail.com>
2026-01-16 17:56:32 -0800

    Refactor ElasticDashboard Subsystem

--- src/main/java/frc/robot/util/ElasticDashboard.java
+++ src/main/java/frc/robot/util/ElasticDashboard.java
@@ -4,31 +4,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ElasticDashboard extends SubsystemBase{
-    SendableChooser<String> menu = new SendableChooser<String>();
-                
-                
-    public ElasticDashboard(){
-        menu.setDefaultOption("name 1","value 1");
-                menu.addOption("name 2", "value 2");
-        SmartDashboard.putData("dropdown",menu);
-        menu.addOption("name 3", "value 3");
-        SmartDashboard.putString("tester", "hello world :)");
+public class ElasticDashboard extends SubsystemBase {
 
+    SendableChooser<String> menu = new SendableChooser<String>();
+
+    public ElasticDashboard() {
+
+        menu.setDefaultOption("name 1", "value 1");
+        menu.addOption("name 2", "value 2");
+        menu.addOption("name 3", "value 3");
+
+        SmartDashboard.putData("dropdown", menu);
+        SmartDashboard.putString("tester", "hello world :)");
 
     }
 
     public String getMenuItem() {
         return menu.getSelected();
-
-
     }
 
     @Override
     public void periodic() {
+
         String tester = SmartDashboard.getString("tester", "");
         SmartDashboard.putString("mirror", tester);
-        
+
         SmartDashboard.putString("selected", menu.getSelected());
     }
 }
