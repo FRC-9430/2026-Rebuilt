83bf970adf7b383b4d246573ff44fbd09bc21c52
IamIisI <titusj915@gmail.com>
2026-01-16 18:11:25 -0800

    Add CANConstants and ControllerConstants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,5 +2,16 @@
 package frc.robot;
 
 public final class Constants {
+    public static final class CANConstants {
+
+    }
+
+    public static final class ControllerConstants {
+        public static final int DRIVER_CONTROLLER_PORT = 0;
+        public static final int OPERATOR_CONTROLLER_PORT = 1;
+        
+        public static final double JOYSTICK_DEADBAND = 0.08;
+        public static final double TRIGGER_DEADBAND = 0.05;
+    }
   
 }
