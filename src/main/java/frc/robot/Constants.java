1202c6a1112ffe54c5875a98bdd2747e7db7031e
PeaceCatis <littlemissamayapie@gmail.com>
2026-01-16 19:09:09 -0800

    Fix merge issues from 76359d97b5c9db10c2cb0caba985d51b5e79c770

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,18 @@ public final class Constants {
         public static final double kI = 0; // TODO: Dummy value
         public static final double kD = 0; // TODO: Dummy value
     }
+
+    public static final class PIDConstants {
+        public static final double kclimberP = 0.001; // TODO: Dummy value
+        public static final double kclimberI = 0; // TODO: Dummy value
+        public static final double kclimberD = 0; // TODO: Dummy value
+    }
+
+
+    public static final class MotorConstants {
+        public static final int ClimberMotor1CanID = 1; //TODO: get motor can ID
+        public static final int ClimberMotor2CanID = 2; //TODO: get motor can ID    
+    }
 }
+
+    
