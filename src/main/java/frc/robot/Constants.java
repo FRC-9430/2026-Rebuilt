1787bbbbf1e92b3e1f9fc4ce39616ad74804212b
IamIisI <titusj915@gmail.com>
2026-01-16 17:54:04 -0800

    Add CAN Constant and more skeleton to Climber Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,10 @@ package frc.robot;
 
 public final class Constants {
 
+    public static final class CANConstants {
+        public static final int kClimbingArmMotorID = 15; // TODO: Dummy value
+    }
+
     public static final class ClimbingArmConstants {
         public static final double kP = 0.001; // TODO: Dummy value
         public static final double kI = 0; // TODO: Dummy value
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,10 +2,31 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.robot.Constants.CANConstants;
 import frc.robot.Constants.ClimbingArmConstants;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
+    
     PIDController climberController = new PIDController(ClimbingArmConstants.kP,ClimbingArmConstants.kI,ClimbingArmConstants.kD);
+    SparkFlex climbingArmMotor = new SparkFlex(CANConstants.kClimbingArmMotorID, MotorType.kBrushless);
+    
+    public Climber() {
+        
+    }
+
+    public void setMotorSpeed(double speed) {
+        climbingArmMotor.set(speed);
+    }
+
+    public void stopMotor() {
+        climbingArmMotor.stopMotor();
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
