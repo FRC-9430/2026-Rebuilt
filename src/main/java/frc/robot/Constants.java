17fb6cef07dd9e2eede0cb966d07ed6b9ec2d734
IamIisI <titusj915@gmail.com>
2026-01-16 17:51:43 -0800

    Repair Error in Constants and remove unused import

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,13 +1,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.controller.PIDController;
-
 public final class Constants {
 
     public static final class ClimbingArmConstants {
         public static final double kP = 0.001; // TODO: Dummy value
         public static final double kI = 0; // TODO: Dummy value
         public static final double kD = 0; // TODO: Dummy value
-    
+    }
 }
