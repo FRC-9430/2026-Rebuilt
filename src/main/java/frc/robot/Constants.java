aa8e7c70abaf2e5865c4852b8c9497bb3d83cd82
IamIisI <titusj915@gmail.com>
2026-01-16 18:25:11 -0800

    Add JavaDoc comments for constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,17 +1,27 @@
 
 package frc.robot;
 
+/**
+ * Container class for other Constants classes
+ */
 public final class Constants {
+
+    /**
+     * CAN IDs for all CAN devices
+     */
     public static final class CANConstants {
 
     }
 
+    /**
+     * Controller related constants
+     */
     public static final class ControllerConstants {
         public static final int DRIVER_CONTROLLER_PORT = 0;
         public static final int OPERATOR_CONTROLLER_PORT = 1;
-        
+
         public static final double JOYSTICK_DEADBAND = 0.08;
         public static final double TRIGGER_DEADBAND = 0.05;
     }
-  
+
 }
