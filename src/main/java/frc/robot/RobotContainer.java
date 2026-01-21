FRC-9430/2026-Rebuilt

commit d20be54d70335cad206dce608c9dd22c1abd98c4

Button configurings for climber

src/main/java/frc/robot/RobotContainer.java
@@ -10,11 +10,14 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.ControllerConstants;
+import frc.robot.subsystems.Climber;
 
 public class RobotContainer {
 
   private CommandXboxController driverController = new CommandXboxController(
       ControllerConstants.DRIVER_CONTROLLER_PORT);
+
+  private Climber robotClimber = new Climber();
 
   public RobotContainer() {
     configureBindings();
@@ -23,7 +26,9 @@ public class RobotContainer {
   /**
    * Configure button bindings.
    *
-   * <p>Use the command-based framework for button bindings. Example usages:
+   * <p>
+   * Use the command-based framework for button bindings. Example usages:
+   * 
    * <pre>{@code
    * // Single-shot
    * driverController.a().onTrue(
@@ -47,6 +52,11 @@ public class RobotContainer {
     driverController.b().whileTrue(new RepeatCommand(new InstantCommand(() -> {
       // Code To Run
     })));
+
+    driverController.x()
+        .whileTrue(new RepeatCommand(new InstantCommand() -> {
+          robotClimber.setclimberMotor1(1) )
+        }.onFalse(new InstantCommand() -> { robotClimber.StopClimber1(1) }));
 
     driverController.rightTrigger(/* Threshold */).whileTrue(new RepeatCommand(new InstantCommand(() -> {
       // drivercontroller.getRightTriggerAxis();

commit 0d4b7091fe409e6f459c0284605cea66eff01fd2

fix syntax errors  introduced in d20be54d70335cad206dce608c9dd22c1abd98c4
Keep in mind: all commands are sequences of commands. Every java statement in a command still needs to end in a semicolon.

src/main/java/frc/robot/RobotContainer.java
@@ -28,7 +28,7 @@ public class RobotContainer {
    *
    * <p>
    * Use the command-based framework for button bindings. Example usages:
-   * 
+   *
    * <pre>{@code
    * // Single-shot
    * driverController.a().onTrue(
@@ -54,9 +54,9 @@ public class RobotContainer {
     })));
 
     driverController.x()
-        .whileTrue(new RepeatCommand(new InstantCommand() -> {
-          robotClimber.setclimberMotor1(1) )
-        }.onFalse(new InstantCommand() -> { robotClimber.StopClimber1(1) }));
+        .whileTrue(new RepeatCommand(new InstantCommand(() -> {
+          robotClimber.setClimberMotor1(1);
+        }))).onFalse(new InstantCommand(() -> { robotClimber.stopClimber1(0); }));
 
     driverController.rightTrigger(/* Threshold */).whileTrue(new RepeatCommand(new InstantCommand(() -> {
       // drivercontroller.getRightTriggerAxis();