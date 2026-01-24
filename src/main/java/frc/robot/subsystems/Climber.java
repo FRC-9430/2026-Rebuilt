a6fbdfb1c149215d18d089c0a2238953ea45a7aa
PeaceCatis <littlemissamayapie@gmail.com>
2026-01-23 17:46:41 -0800

    Removed pseudo code Added basic notes Made formant look good

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,78 +1,63 @@
 package frc.robot.subsystems;
 
+//Import packages (thumbs up)
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkFlex;
-
 import frc.robot.Constants.ClimberArmConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANConstants;
 
+// create climber
 public class Climber extends SubsystemBase {
-
 
     private SparkFlex climberMotor1;
     private SparkFlex climberMotor2;
     private AbsoluteEncoder climberEncoder1;
     private AbsoluteEncoder climberEncoder2;
 
-    public  Climber() {
-
+    // Declare the climber
+    public Climber() {
+        // Declare motors and their encoders
         climberMotor1 = new SparkFlex(CANConstants.climberMotor1CanID, SparkFlex.MotorType.kBrushless);
         climberMotor2 = new SparkFlex(CANConstants.climberMotor2CanID, SparkFlex.MotorType.kBrushless);
         climberEncoder1 = climberMotor1.getAbsoluteEncoder();
         climberEncoder2 = climberMotor2.getAbsoluteEncoder();
     }
 
+    // stop climber command
     public void stopClimber() {
         climberMotor1.stopMotor();
         climberMotor2.stopMotor();
     }
 
-
+    // Verify motors are not above or below the limits
     public boolean climberMotor1Valid() {
         if (climberEncoder1.getPosition() > ClimberArmConstants.kClimberMax
-                && climberEncoder1.getPosition() < ClimberArmConstants.kClimberMin);
-            return true;
+                && climberEncoder1.getPosition() < ClimberArmConstants.kClimberMin)
+            ;
+        return true;
     }
 
     public boolean climberMotor2Valid() {
         if (climberEncoder2.getPosition() > ClimberArmConstants.kClimberMax
-                && climberEncoder2.getPosition() < ClimberArmConstants.kClimberMin);
-            return true;
+                && climberEncoder2.getPosition() < ClimberArmConstants.kClimberMin)
+            ;
+        return true;
     }
 
-
-    public void setClimberMotors(double speed){
+    // If both encoders valid set climber motors
+    public void setClimberMotors(double speed) {
         if (climberMotor1Valid() == true
-                && climberMotor2Valid() == true);
-           setClimberMotors(speed);
-        
-
-
+                && climberMotor2Valid() == true)
+            ;
+        setClimberMotors(speed);
     }
 
-    /*
-     * public boolean isSetpointValid(double setpoint) {
-     *  if (setpoint is greater than climbing arm minimum
-     *      and setpoint is less than climbing arm maximum) {
-     *          then the setpoint is valid
-     *      }
-     *  else // setpoint is not valid {
-     *          then the setpoint is invalid
-     *      }
-     *  }
-     *
-     * public void setSetpoint(setpoint) {
-     *  if the setpoint is valid,
-     *      then this.setpoint = setpoint // keep in mind, this.anything requires a pre-defined instance variable
-     * }
-     *
-     */
+    // get encoder postion periodicly
     @Override
     public void periodic() {
         climberEncoder1.getPosition();
         climberEncoder2.getPosition();
 
-        
     }
 }
