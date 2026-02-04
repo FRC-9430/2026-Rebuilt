f72bdf1c31016c6ed4e3bce87e6272af78cc12ba
PeaceCatis <littlemissamayapie@gmail.com>
2026-02-03 17:03:18 -0800

    finish merge

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,6 +10,22 @@ public final class Constants {
      * CAN IDs for all CAN devices
      */
     public static final class CANConstants {
+        public static final int ROBORIO_CAN_ID = 0;
+        public static final int PIGEON2_CAN_ID = 1;
+        public static final int REV_PDH_CAN_ID = 2;
+        public static final int FL_SWERVE_ENCODER_CAN_ID = 6;
+        public static final int FR_SWERVE_ENCODER_CAN_ID = 7;
+        public static final int BL_SWERVE_ENCODER_CAN_ID = 8;
+        public static final int BR_SWERVE_ENCODER_CAN_ID = 9;
+        public static final int FL_SWERVE_TURNING_CAN_ID = 10;
+        public static final int FR_SWERVE_TURNING_CAN_ID = 11;
+        public static final int BL_SWERVE_TURNING_CAN_ID = 12;
+        public static final int BR_SWERVE_TURNING_CAN_ID = 13;
+        public static final int FL_SWERVE_DRIVING_CAN_ID = 14;
+        public static final int FR_SWERVE_DRIVING_CAN_ID = 15;
+        public static final int BL_SWERVE_DRIVING_CAN_ID = 16;
+        public static final int BR_SWERVE_DRIVING_CAN_ID = 17;
+
         public static final int climberMotor1CanID = 21; //TODO: get motor can ID
         public static final int climberMotor2CanID = 22; //TODO: get motor can ID
     }
@@ -24,12 +40,12 @@ public final class Constants {
         public static final double JOYSTICK_DEADBAND = 0.08;
         public static final double TRIGGER_DEADBAND = 0.05;
     }
-  
+
     /**
     * Constants for the Climbing Arm Subsystem
     */
     public static final class ClimberArmConstants {
-    
+
         public static final double kClimberMax = .48; //TODO: DummyValue
         public static final double kClimberMin = .090;//TODO: DummyValue
     }
