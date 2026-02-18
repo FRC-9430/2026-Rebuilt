57e84fef3ee9a8385f04e58ea17937e51ca55b54
FRCteam9430 <frc.9430@outlook.com>
2026-02-17 18:01:07 -0800

    Add More Camera Param

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
+
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.LimelightHelpers;
@@ -12,35 +15,54 @@ public class VisionSubsystem extends SubsystemBase {
     public VisionSubsystem(CommandSwerveDrivetrain drive) {
         drivetrain = drive;
 
-        drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.5, .5, 9999999));
+        drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 9999999));
+
+        LimelightHelpers.SetIMUMode("limelight", 1);
     }
 
-    public LimelightHelpers.PoseEstimate getPoseEstimate() {
+    public LimelightHelpers.PoseEstimate getPoseEstimateMT2() {
+        return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+    }
+
+    public LimelightHelpers.PoseEstimate getPoseEstimateMT1() {
         return LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight");
     }
 
     public void addVisionMeasurements() {
-        LimelightHelpers.PoseEstimate poseEstimate = getPoseEstimate();
-        SmartDashboard.putNumber("Robot Pose Cam Est X", poseEstimate.pose.getX());
-        SmartDashboard.putNumber("Robot Pose Cam Est Y", poseEstimate.pose.getY());
+        SwerveDriveState driveState = drivetrain.getState();
+
+        LimelightHelpers.SetRobotOrientation(
+            "limelight", 
+            driveState.Pose.getRotation().getDegrees(),
+            0, 0, 0, 0, 0);
+
+        LimelightHelpers.PoseEstimate poseEstimateMT1 = getPoseEstimateMT1();
+        LimelightHelpers.PoseEstimate poseEstimateMT2 = getPoseEstimateMT2();
+
+        SmartDashboard.putNumber("Robot Pose Cam Est X", poseEstimateMT1.pose.getX());
+        SmartDashboard.putNumber("Robot Pose Cam Est Y", poseEstimateMT1.pose.getY());
+
 
         boolean doRejectUpdate = false;
-        if (poseEstimate.tagCount == 1 && poseEstimate.rawFiducials.length == 1) {
-            if (poseEstimate.rawFiducials[0].ambiguity > .5) {
+        if (poseEstimateMT1.tagCount == 1 && poseEstimateMT1.rawFiducials.length == 1) {
+            if (poseEstimateMT1.rawFiducials[0].ambiguity > .7) {
                 doRejectUpdate = true;
             }
-            if (poseEstimate.rawFiducials[0].distToCamera > 3) {
+            if (poseEstimateMT1.rawFiducials[0].distToCamera > 3) {
                 doRejectUpdate = true;
             }
         }
-        if (poseEstimate.tagCount == 0) {
+        if (poseEstimateMT1.tagCount == 0) {
             doRejectUpdate = true;
         }
 
+        Pose2d combPose = new Pose2d(poseEstimateMT1.pose.getTranslation(), poseEstimateMT2.pose.getRotation());
+
+
         if (!doRejectUpdate) {
             drivetrain.addVisionMeasurement(
-                    poseEstimate.pose,
-                    poseEstimate.timestampSeconds);
+                    poseEstimateMT1.pose,
+                    poseEstimateMT1.timestampSeconds);
         }
 
     }
