eee4f823914a1ca7ba32be35298dbe1e6c726311
FRCteam9430 <frc.9430@outlook.com>
2026-02-17 18:45:51 -0800

    Add Commands for ejection/retraction

---
+++ src/main/java/frc/robot/commands/RetractBasketCommand.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class RetractBasketCommand extends Command {
+
+  final IntakeSubsystem intake;
+  double startTime;
+
+  /** Creates a new RetractBasketCommand. */
+  public RetractBasketCommand(IntakeSubsystem intake) {
+    addRequirements(intake);
+    this.intake = intake;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startTime = Timer.getFPGATimestamp();
+
+    intake.setBasket(0.2);
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.stopBasket();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (Timer.getFPGATimestamp() > (startTime + 0.8)); // Run 0.8 second
+  }
+}
