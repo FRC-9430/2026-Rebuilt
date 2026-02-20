cca2c14aa9e0723fed2740430fadfceefba5ea2b
FRCteam9430 <frc.9430@outlook.com>
2026-02-19 19:25:46 -0800

    Configure Auto Chooser

--- src/main/java/frc/robot/util/ElasticDashboard.java
+++ src/main/java/frc/robot/util/ElasticDashboard.java
@@ -1,13 +1,17 @@
 package frc.robot.util;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElasticDashboard extends SubsystemBase {
 
     SendableChooser<Pose2d> initialPoseChooser = new SendableChooser<Pose2d>();
+    SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
 
     public ElasticDashboard() {
 
@@ -20,10 +24,16 @@ public class ElasticDashboard extends SubsystemBase {
 
         SmartDashboard.putData("InitialPose", initialPoseChooser);
 
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+
     }
 
     public Pose2d getInitialPose() {
         return initialPoseChooser.getSelected();
+    }
+
+    public Command getAutoChooser() {
+        return autoChooser.getSelected();
     }
 
     @Override
