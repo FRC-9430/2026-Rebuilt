cb6ebd0b24a032634963995879f6556a1550d324
Brady Bontrager <bbontrager89@gmail.com>
2026-02-24 21:53:42 -0800

    Delete outdated test cases (see pr #53)

--- src/test/java/frc/robot/subsystems/IntakeSubsystemTest.java
+++ src/test/java/frc/robot/subsystems/IntakeSubsystemTest.java
@@ -53,25 +53,4 @@ class IntakeSubsystemTest {
             m_intake = null;
         }
     }
-
-    @Test
-    @Disabled
-    void testIntakeAndConveyorRunTogether() {
-        // Engage the intake
-        // subsystem.setSpeeds(0.5, 0.5);
-
-        // Verify both motors are set to the same speed
-        assertEquals(0.5, m_intake.intakeMotor.get(), 0.001, "Intake motor should be at 0.5 speed");
-        // assertEquals(0.5, subsystem.conveyorMotor.get(), 0.001, "Conveyor motor
-        // should be at 0.5 speed");
-    }
-
-    @Test
-    @Disabled
-    void testStopDisengagesBoth() {
-        m_intake.stopAll();
-        assertEquals(0.0, m_intake.intakeMotor.get(), 0.001, "Intake motor should be stopped");
-        // assertEquals(0.0, subsystem.conveyorMotor.get(), 0.001, "Conveyor motor
-        // should be stopped");
-    }
 }
