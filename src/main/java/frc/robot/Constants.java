54ae44aa25c6cfc325bf64077ede91967180bee6
FRCteam9430 <frc.9430@outlook.com>
2026-04-06 16:45:55 -0700

    Increase Intake Speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -265,8 +265,8 @@ public final class Constants {
 
     public static final class IntakeConstants {
 
-        public static final double kDefaultAutoIntakeSpeed = 83;
-        public static final double kDefaultTeleIntakeSpeed = 75;
+        public static final double kDefaultAutoIntakeSpeed = 100;
+        public static final double kDefaultTeleIntakeSpeed = 83;
 
         public static final double kIntakeP = 0.0;
         public static final double kIntakeI = 0.0;
